227e9026c53d4ac4ecbecd9173d5c46042874e91
Thierry Lelegard <thierry@lelegard.fr>
2018-01-11 22:19:50 +0100

    Added HEVC tests

---
+++ tests/test-010.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Test various analyses on a live stream with HEVC.
+
+source $(dirname $0)/../common/testrc.sh
+test_cleanup "$SCRIPT.*"
+source "$COMMONDIR"/standard-ts-test.sh test-010.ts
