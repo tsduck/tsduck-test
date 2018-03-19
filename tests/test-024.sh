f68208085418470d925d202a0b7406794884a3a4
Thierry Lelegard <thierry@lelegard.fr>
2018-03-19 22:46:03 +0100

    Version 3.10-624

---
+++ tests/test-024.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Test continuity plugin.
+
+source $(dirname $0)/../common/testrc.sh
+test_cleanup "$SCRIPT.*"
+
+INFILE="$INDIR/$SCRIPT.ts"
+
+$(tspath tsp) --synchronous-log \
+    -I file $(fpath "$INFILE") \
+    -P continuity \
+    -O drop \
+    >"$OUTDIR/$SCRIPT.tsp.1.log" 2>&1
+
+test_text $SCRIPT.tsp.1.log
+
+$(tspath tsp) --synchronous-log \
+    -I file $(fpath "$INFILE") \
+    -P continuity --fix \
+    -P continuity \
+    -O file $(fpath "$OUTDIR/$SCRIPT.out.ts") \
+    >"$OUTDIR/$SCRIPT.tsp.2.log" 2>&1
+
+test_bin $SCRIPT.out.ts
+test_text $SCRIPT.tsp.2.log
+
+$(tspath tsdump) $(fpath "$OUTDIR/$SCRIPT.out.ts") >"$OUTDIR/$SCRIPT.tsdump.txt" 2>"$OUTDIR/$SCRIPT.tsdump.log"
+
+test_text $SCRIPT.tsdump.txt
+test_text $SCRIPT.tsdump.log
