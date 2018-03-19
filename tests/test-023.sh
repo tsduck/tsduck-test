95e16427e380f4f500c135863b28fbfaac815ffd
Thierry Lelegard <thierry@lelegard.fr>
2018-03-19 22:18:54 +0100

    Version 3.10-623

---
+++ tests/test-023.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Test file input plugin with more than one file.
+
+source $(dirname $0)/../common/testrc.sh
+test_cleanup "$SCRIPT.*"
+
+INFILE="$INDIR/$SCRIPT"
+
+$(tspath tsp) --synchronous-log \
+    -I file $(fpath "${INFILE}a.ts") $(fpath "${INFILE}b.ts") $(fpath "${INFILE}c.ts") \
+    -O file $(fpath "$OUTDIR/$SCRIPT.1.ts") \
+    >"$OUTDIR/$SCRIPT.tsp.1.log" 2>&1
+
+test_bin $SCRIPT.1.ts
+test_text $SCRIPT.tsp.1.log
+
+$(tspath tsdump) $(fpath "$OUTDIR/$SCRIPT.1.ts") >"$OUTDIR/$SCRIPT.tsdump.1.txt" 2>"$OUTDIR/$SCRIPT.tsdump.1.log"
+
+test_text $SCRIPT.tsdump.1.txt
+test_text $SCRIPT.tsdump.1.log
+
+$(tspath tsp) --synchronous-log \
+    -I file --repeat 2 $(fpath "${INFILE}a.ts") $(fpath "${INFILE}b.ts") $(fpath "${INFILE}c.ts") \
+    -O file $(fpath "$OUTDIR/$SCRIPT.2.ts") \
+    >"$OUTDIR/$SCRIPT.tsp.2.log" 2>&1
+
+test_bin $SCRIPT.2.ts
+test_text $SCRIPT.tsp.2.log
+
+$(tspath tsdump) $(fpath "$OUTDIR/$SCRIPT.2.ts") >"$OUTDIR/$SCRIPT.tsdump.2.txt" 2>"$OUTDIR/$SCRIPT.tsdump.2.log"
+
+test_text $SCRIPT.tsdump.2.txt
+test_text $SCRIPT.tsdump.2.log
