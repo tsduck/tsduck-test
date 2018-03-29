d9f2682a05643f612dcd0029bf41fba1ef3e0a9d
Thierry Lelegard <thierry@lelegard.fr>
2018-03-29 22:25:12 +0200

    Version 3.10-647

---
+++ tests/test-025.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Non-regression test on a stream which crashed tsanalyzer (issue #49).
+
+source $(dirname $0)/../common/testrc.sh
+test_cleanup "$SCRIPT.*"
+source "$COMMONDIR"/standard-ts-test.sh $SCRIPT.ts
