6ef855cf4129ba3c07d53b475052fa6058d572f8
Thierry Lelegard <thierry@lelegard.fr>
2018-04-02 23:53:01 +0200

    Version 3.10-655

---
+++ tests/test-026.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Scrambling test
+
+source $(dirname $0)/../common/testrc.sh
+test_cleanup "$SCRIPT.*"
+
+INFILE="$INDIR/$SCRIPT.ts"
+
+# DVB-CSA scrambling / descrambling.
+$(tspath tsp) --synchronous-log \
+    -I file $(fpath "$INFILE") \
+    -P scrambler 0x0101 -c 0123456789ABCDEF \
+    -P analyze -o $(fpath "$OUTDIR/$SCRIPT.csa.analyze.txt") \
+    -P psi -a -o $(fpath "$OUTDIR/$SCRIPT.csa.psi.txt") \
+    -P file $(fpath "$OUTDIR/$SCRIPT.csa.ts") \
+    -P descrambler -c 0123456789ABCDEF \
+    -P analyze -o $(fpath "$OUTDIR/$SCRIPT.decsa.analyze.txt") \
+    -P psi -a -o $(fpath "$OUTDIR/$SCRIPT.decsa.psi.txt") \
+    -O file $(fpath "$OUTDIR/$SCRIPT.decsa.ts") \
+    >"$OUTDIR/$SCRIPT.tsp.csa.log" 2>&1
+
+test_bin $SCRIPT.csa.ts
+test_bin $SCRIPT.decsa.ts
+test_text $SCRIPT.csa.analyze.txt
+test_text $SCRIPT.decsa.analyze.txt
+test_text $SCRIPT.csa.psi.txt
+test_text $SCRIPT.decsa.psi.txt
+test_text $SCRIPT.tsp.csa.log
+
+# ATIS-IDSA scrambling / descrambling.
+$(tspath tsp) --synchronous-log \
+    -I file $(fpath "$INFILE") \
+    -P scrambler 0x0101 -c 0123456789ABCDEFFEDCBA9876543210 --atis-idsa \
+    -P analyze -o $(fpath "$OUTDIR/$SCRIPT.atis.analyze.txt") \
+    -P psi -a -o $(fpath "$OUTDIR/$SCRIPT.atis.psi.txt") \
+    -P file $(fpath "$OUTDIR/$SCRIPT.atis.ts") \
+    -P descrambler -c 0123456789ABCDEFFEDCBA9876543210 --atis-idsa \
+    -P analyze -o $(fpath "$OUTDIR/$SCRIPT.deatis.analyze.txt") \
+    -P psi -a -o $(fpath "$OUTDIR/$SCRIPT.deatis.psi.txt") \
+    -O file $(fpath "$OUTDIR/$SCRIPT.deatis.ts") \
+    >"$OUTDIR/$SCRIPT.tsp.atis.log" 2>&1
+
+test_bin $SCRIPT.atis.ts
+test_bin $SCRIPT.deatis.ts
+test_text $SCRIPT.atis.analyze.txt
+test_text $SCRIPT.deatis.analyze.txt
+test_text $SCRIPT.atis.psi.txt
+test_text $SCRIPT.deatis.psi.txt
+test_text $SCRIPT.tsp.atis.log
