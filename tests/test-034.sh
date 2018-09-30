13f6521ffc342e1129c80d7bfa9f446ebe244569
Thierry Lelegard <thierry@lelegard.fr>
2018-09-30 23:16:05 +0200

    Added some tsswitch tests

--- tests/test-034.sh
+++ tests/test-034.sh
@@ -8,6 +8,9 @@ INFILE1="$INDIR/test-023a.ts"
 INFILE2="$INDIR/test-023b.ts"
 INFILE3="$INDIR/test-023c.ts"
 
+# URL of test patterns at tsduck.io:
+TESTURL=https://tsduck.io/streams/test-patterns
+
 # Simple file concatenation.
 $(tspath tsswitch) --synchronous-log \
     -I file $(fpath "$INFILE1") \
@@ -29,3 +32,25 @@ $(tspath tsswitch) --synchronous-log --cycle 2 \
 
 test_bin $SCRIPT.2.ts
 test_text $SCRIPT.2.log
+
+# Same with created processes.
+$(tspath tsswitch) --synchronous-log --cycle 2 \
+    -I fork 'tsp -I file '$(fpath "$INFILE1") \
+    -I fork 'tsp -I file '$(fpath "$INFILE2") \
+    -I fork 'tsp -I file '$(fpath "$INFILE3") \
+    -O file $(fpath "$OUTDIR/$SCRIPT.3.ts") \
+    >"$OUTDIR/$SCRIPT.3.log" 2>&1
+
+test_bin $SCRIPT.3.ts
+test_text $SCRIPT.3.log
+
+# Same with HTTPS files (requires network access to tsduck.io).
+$(tspath tsswitch) --synchronous-log --cycle 2 \
+    -I http $TESTURL/test-1packet-01.ts \
+    -I http $TESTURL/test-2packets-02-03.ts \
+    -I http $TESTURL/test-3packets-04-05-06.ts \
+    -O file $(fpath "$OUTDIR/$SCRIPT.4.ts") \
+    >"$OUTDIR/$SCRIPT.4.log" 2>&1
+
+test_bin $SCRIPT.4.ts
+test_text $SCRIPT.4.log
