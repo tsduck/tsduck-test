8d1796aa94a77a04ba6e0c809cddf8c4e96624fb
Thierry Lelegard <thierry@lelegard.fr>
2019-03-23 15:46:08 +0100

    Added ATSC test streams

---
+++ tests/test-040.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Test various analyses on an ATSC live stream.
+
+source $(dirname $0)/../common/testrc.sh
+test_cleanup "$SCRIPT.*"
+source "$COMMONDIR"/standard-ts-test.sh $SCRIPT.ts
