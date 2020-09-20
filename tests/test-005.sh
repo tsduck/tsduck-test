1ac5939f2aad90cfd3daa4c3f7024eab1fd09d89
Thierry Lelegard <thierry@lelegard.fr>
2020-09-20 22:13:30 +0200

    Version 1994

--- tests/test-005.sh
+++ tests/test-005.sh
@@ -3,17 +3,4 @@
 
 source $(dirname $0)/../common/testrc.sh
 test_cleanup "$SCRIPT.*"
-
-INFILE="$INDIR/test-005.xml"
-
-$(tspath tstabcomp) $(fpath "$INFILE") --output $(fpath "$OUTDIR/$SCRIPT.compile.bin") \
-    2>"$OUTDIR/$SCRIPT.compile.log" 2>&1
-
-test_bin $SCRIPT.compile.bin
-test_text $SCRIPT.compile.log
-
-$(tspath tstabcomp) $(fpath "$OUTDIR/$SCRIPT.compile.bin") --decompile --output $(fpath "$OUTDIR/$SCRIPT.decompile.xml") \
-    2>"$OUTDIR/$SCRIPT.decompile.log" 2>&1
-
-test_text $SCRIPT.decompile.xml
-test_text $SCRIPT.decompile.log
+source "$COMMONDIR"/standard-si-test.sh $SCRIPT.xml
