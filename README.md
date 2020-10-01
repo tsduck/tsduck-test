cf33b4d6787b1787ef90e64b7010c9ef62c9e28b
Thierry Lelegard <thierry@lelegard.fr>
2020-10-01 22:23:09 +0200

    Added non-regression test-072 for issue #646

--- README.md
+++ README.md
@@ -166,3 +166,4 @@ repository. But we now limit their size to 20 MB.
 | 069  | Non-regression on `tsecmg` (issue #619)
 | 070  | Non-regression on `fork` packet processor plugin (issue #633)
 | 071  | RS204 file format
+| 072  | Non-regression on `pes` plugin (issue #646)
---
+++ tests/test-072.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Non-regression on pes plugin (issue #646)
+
+source $(dirname $0)/../common/testrc.sh
+test_cleanup "$SCRIPT.*"
+
+source "$COMMONDIR"/standard-ts-test.sh $SCRIPT.ts
+
+$(tspath tsp) --synchronous-log \
+    -I file $(fpath "$INDIR/$SCRIPT.ts") \
+    -P pes --packet-index --header \
+    -O drop \
+    >"$OUTDIR/$SCRIPT.pes.log" 2>&1
+
+test_text $SCRIPT.pes.log
