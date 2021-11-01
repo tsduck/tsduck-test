c3ac6dbaaf0407b99d291eac6d7c73331d39c097
Thierry Lelegard <thierry@lelegard.fr>
2021-11-01 18:46:35 +0100

    Version 2623

--- tests/test-084.sh
+++ tests/test-084.sh
@@ -16,9 +16,21 @@ $(tspath tsp) --synchronous-log \
     -I file $(fpath "$INDIR/$SCRIPT.ts") \
     -P splicemonitor --packet-index --display-commands -o $(fpath "$OUTDIR/$SCRIPT.2.txt") \
     -P splicemonitor --all-commands --splice-pid 1055 -o $(fpath "$OUTDIR/$SCRIPT.3.txt") \
+    -P splicemonitor --all-commands --json -o $(fpath "$OUTDIR/$SCRIPT.4.json") \
+    -P splicemonitor --json -o $(fpath "$OUTDIR/$SCRIPT.5.json") \
     -O drop \
     >"$OUTDIR/$SCRIPT.2.log" 2>&1
 
 test_text $SCRIPT.2.log
 test_text $SCRIPT.2.txt
 test_text $SCRIPT.3.txt
+test_text $SCRIPT.4.json
+test_text $SCRIPT.5.json
+
+$(tspath tsp) --synchronous-log \
+    -I file $(fpath "$INDIR/$SCRIPT.ts") \
+    -P splicemonitor --json-line=FOO: \
+    -O drop \
+    >"$OUTDIR/$SCRIPT.6.log" 2>&1
+
+test_text $SCRIPT.6.log
