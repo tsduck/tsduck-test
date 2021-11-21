65b09b91eb0fc7c414eb4359f87b8bf5eeba0389
Thierry Lelegard <thierry@lelegard.fr>
2021-11-21 22:44:22 +0100

    Version 2641

--- tests/test-106.sh
+++ tests/test-106.sh
@@ -12,3 +12,13 @@ $(tspath tspcap) $(fpath "$INDIR/$SCRIPT.2.pcapng") --list-streams \
 
 test_text $SCRIPT.list.1.log
 test_text $SCRIPT.list.2.log
+
+$(tspath tspcap) $(fpath "$INDIR/$SCRIPT.2.pcapng") --dvb-simulcrypt --udp \
+    >"$OUTDIR/$SCRIPT.list.3.log" 2>&1
+
+test_text $SCRIPT.list.3.log
+
+$(tspath tspcap) $(fpath "$INDIR/$SCRIPT.1.pcapng") --dvb-simulcrypt \
+    >"$OUTDIR/$SCRIPT.list.4.log" 2>&1
+
+test_text $SCRIPT.list.4.log
