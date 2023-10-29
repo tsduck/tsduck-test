b2a728e14b5bf8f46b9c16bc11fcefad503d5215
Thierry Lelegard <thierry@lelegard.fr>
2023-10-29 23:56:24 +0100

    Version 3357

--- tests/test-135.sh
+++ tests/test-135.sh
@@ -40,3 +40,13 @@ $(tspath tspcap) $(fpath "$INDIR/$SCRIPT.rtsp.pcap") --source 10.31.51.78:554 \
 
 test_bin $SCRIPT.rtsp.tcp.bin
 test_text $SCRIPT.rtsp.tcp.log
+
+test_tsp \
+    -I pcap $(fpath "$INDIR/$SCRIPT.seqwrap.pcap") --http --source 10.31.51.78:554 \
+    -P analyze -o $(fpath "$OUTDIR/$SCRIPT.seqwrap.txt") \
+    -O file $(fpath "$OUTDIR/$SCRIPT.seqwrap.ts") \
+    >"$OUTDIR/$SCRIPT.seqwrap.log" 2>&1
+
+test_bin $SCRIPT.seqwrap.ts
+test_text $SCRIPT.seqwrap.txt
+test_text $SCRIPT.seqwrap.log
