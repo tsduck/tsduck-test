6e4eba567d75d318321d0f3d8289559680a7da04
Thierry Lelegard <thierry@lelegard.fr>
2024-09-11 22:58:12 +0200

    Version 3852

--- tests/test-160.sh
+++ tests/test-160.sh
@@ -3,15 +3,4 @@
 
 source $(dirname $0)/../common/testrc.sh
 test_cleanup "$SCRIPT.*"
-
-$(tspath tstabcomp) $(fpath "$INDIR/$SCRIPT.xml") \
-    -o $(fpath "$OUTDIR/$SCRIPT.bin") \
-    >"$OUTDIR/$SCRIPT.tstabcomp.log" 2>&1
-
-test_text $SCRIPT.tstabcomp.log
-test_bin $SCRIPT.bin
-
-$(tspath tstabdump) $(fpath "$OUTDIR/$SCRIPT.bin") \
-    >"$OUTDIR/$SCRIPT.tstabdump.log" 2>&1
-
-test_text $SCRIPT.tstabdump.log
+source "$COMMONDIR"/standard-si-test.sh $SCRIPT.xml
