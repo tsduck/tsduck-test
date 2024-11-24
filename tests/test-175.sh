296e0d0d8c51de5feb6edb2e212431ee800e2859
Thierry Lelegard <thierry@lelegard.fr>
2024-11-24 23:36:58 +0100

    Version 3970

--- tests/test-175.sh
+++ tests/test-175.sh
@@ -7,13 +7,7 @@ test_cleanup "$SCRIPT.*"
 INFILE="$INDIR/test-092.ts"
 
 $(tspath tsfclean) $(fpath "$INFILE") -o - 2>"$OUTDIR/$SCRIPT.tsfclean.log" |
-    tsp -P history -O drop >"$OUTDIR/$SCRIPT.tsp.log" 2>&1
+    test_tsp -P history -O drop >"$OUTDIR/$SCRIPT.tsp.log" 2>&1
 
 test_text $SCRIPT.tsfclean.log
 test_text $SCRIPT.tsp.log
-
-grep '* history:' "$OUTDIR/$SCRIPT.tsp.log" |
-    sort -t : -k 2 -n \
-    >"$OUTDIR/$SCRIPT.history.log" 2>&1
-
-test_text $SCRIPT.history.log
