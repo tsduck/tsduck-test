5730e58aa7bff445ade582a36ffc9dd6a312f10c
Thierry Lelegard <thierry@lelegard.fr>
2025-04-26 15:49:05 +0200

    Version 4259

--- tests/test-187.sh
+++ tests/test-187.sh
@@ -37,3 +37,18 @@ test_tsp \
     >"$OUTDIR/$SCRIPT.4.log" 2>&1
 
 test_text $SCRIPT.4.log
+
+test_tsp \
+    -I file $(fpath "$INFILE") \
+    -P filter --service france2 --set-label 2 \
+    -P tables --only-label 2 --tid 2 --text-output $(fpath "$OUTDIR/$SCRIPT.5.pmt1.txt") \
+    -P identify --service france2 --video --set-environment-variable VIDEO_PID --all-set-label 3 \
+    -P pmt --only-label 3 --service france2 --increment-version \
+           --patch-xml $(fpath "$INDIR/$SCRIPT.patch.xml") --expand-patch-xml \
+    -P tables --only-label 2 --tid 2 --text-output $(fpath "$OUTDIR/$SCRIPT.5.pmt2.txt") \
+    -O drop \
+    >"$OUTDIR/$SCRIPT.5.log" 2>&1
+
+test_text $SCRIPT.5.log
+test_text $SCRIPT.5.pmt1.txt
+test_text $SCRIPT.5.pmt2.txt
