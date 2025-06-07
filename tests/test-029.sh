0ed30f00e40d8d2412ee04aea877bbcda950c6f0
Thierry Lelegard <thierry@lelegard.fr>
2025-06-07 23:53:38 +0200

    Version 4320

--- tests/test-029.sh
+++ tests/test-029.sh
@@ -10,7 +10,7 @@ for bw in 8-MHz 7-MHz 6-MHz 5-MHz; do
     for co in QPSK 16-QAM 64-QAM; do
         for gi in 1/32 1/16 1/8 1/4; do
             for fec in 1/2 2/3 3/4 5/6 7/8; do
-                echo -n "BW: $bw, constel: $co, guard: $gi, FEC: $fec, bandwidth: "
+                echo -n "BW: $bw, constel: $co, guard: $gi, FEC: $fec, bitrate: "
                 $(tspath tsterinfo) -s -w $bw -c $co -g $gi -h $fec
             done
         done
