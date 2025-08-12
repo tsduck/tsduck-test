6acf3cc039c9b823203e882b56fd7483082f5fd1
Thierry Lelegard <thierry@lelegard.fr>
2025-08-12 22:22:25 +0200

    Version 4397

--- tests/test-193.sh
+++ tests/test-193.sh
@@ -7,22 +7,30 @@ test_cleanup "$SCRIPT.*"
 INFILE="$INDIR/test-001.ts"
 INFLUX_PORT=47852
 
-$(tspath tsdebug) server :$INFLUX_PORT --max-clients 3 \
-    --sort-headers --hide-header Connection --hide-header Cache-Control \
-    >"$OUTDIR/$SCRIPT.server.log" 2>&1 &
+influx_test()
+{
+    local name=$1
 
-server_pid=$!
-sleep 0.5
+    $(tspath tsdebug) server :$INFLUX_PORT --max-clients 3 \
+        --sort-headers --hide-header Connection --hide-header Cache-Control \
+        >"$OUTDIR/$SCRIPT.server.$name.log" 2>&1 &
 
-test_tsp \
-    -I file --infinite $(fpath "$INFILE") \
-    -P influx --interval 3 --max-metrics 3 --pcr-based --start-time 2025/07/14:12:34:56 \
-              --all-pids --services --names --type \
-              --host-url http://localhost:$INFLUX_PORT/ --bucket test-bucket --org test-org --token test-token \
-    -O drop \
-    >"$OUTDIR/$SCRIPT.tsp.log" 2>&1
+    server_pid=$!
+    sleep 0.5
 
-wait $server_pid
+    test_tsp \
+        -I file --infinite $(fpath "$INFILE") \
+        -P influx $2 --interval 3 --max-metrics 3 --pcr-based --start-time 2025/07/14:12:34:56 \
+                  --host-url http://localhost:$INFLUX_PORT/ --bucket test-bucket --org test-org --token test-token \
+        -O drop \
+        >"$OUTDIR/$SCRIPT.tsp.$name.log" 2>&1
 
-test_text $SCRIPT.server.log
-test_text $SCRIPT.tsp.log
+    wait $server_pid
+
+    test_text $SCRIPT.server.$name.log
+    test_text $SCRIPT.tsp.$name.log
+}
+
+influx_test bitrate  "--all-pids --services --names --type"
+influx_test pcr      "--pid 200-250 --services --names --pcr --pts --dts"
+influx_test ts101290 "--tr-101-290"
