8a9d09513fb780c198f707f6a8b2ebc3f90a9685
Thierry Lelegard <thierry@lelegard.fr>
2025-12-22 21:42:49 +0100

    Version 4538

--- README.md
+++ README.md
@@ -295,3 +295,4 @@ repository. But we now limit their size to 20 MB.
 | 198  | Plugin `nip`
 | 199  | `tsflute`
 | 200  | Plugin `flute`
+| 201  | SI analysis and conversions: DTMB signalization
--- tests/test-201.sh
+++ tests/test-201.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Test descriptors
+# Test descriptors: DTMB
 
 source $(dirname $0)/../common/testrc.sh
 test_cleanup "$SCRIPT.*"
-source "$COMMONDIR"/standard-si-test.sh $SCRIPT.xml --china
+source "$COMMONDIR"/standard-si-test.sh $SCRIPT.xml --dtmb
